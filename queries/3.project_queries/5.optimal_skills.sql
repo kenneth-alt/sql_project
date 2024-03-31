3450d31d66e638e248f14231ad1d5a9efbe43ec3
kenneth-alt <kenneth.ugo15@gmail.com>
2024-03-31 16:21:44 -0600

    add queries

--- queries/3.project_queries/5.optimal_skills.sql
+++ queries/3.project_queries/5.optimal_skills.sql
@@ -27,7 +27,6 @@ WITH skills_demand AS (
     skills_job_dim.skill_id
 )
 
-
 SELECT
   skills_demand.skill_id,
   skills_demand.skills,
@@ -39,4 +38,22 @@ INNER JOIN average_salary ON skills_demand.skill_id = average_salary.skill_id
 ORDER BY
   demand_count DESC,
   avg_salary DESC
+LIMIT 25;
+
+-- Make the above query more concise
+SELECT
+  skills_dim.skill_id,
+  skills_dim.skills,
+  COUNT(skills_job_dim.job_id) AS demand_count,
+  ROUND(AVG(job_postings_fact.salary_year_avg), 0) AS avg_salary
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+  job_title_short = 'Data Engineer'
+  AND salary_year_avg IS NOT NULL
+GROUP BY
+  skills_dim.skill_id
+ORDER BY 
+  demand_count DESC
 LIMIT 25;
